77ff51614d7370244e5e51eb9e90b6f3feecbcca
Miguel Cabrera <miguel.cabrera@system73.com>
2021-09-01 11:55:13 +0100

    feat: Add title for API

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from api.v1.router import api_v1_router
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Sample Project")
 
 
 app.include_router(api_v1_router, prefix="/api/v1")
