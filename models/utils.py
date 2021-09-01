bb9378bdbfaef3ce69b5a60b295b563714f2d2b2
Miguel Cabrera <miguel.cabrera@system73.com>
2021-09-01 11:38:57 +0100

    fix: Fix how extend a pydantic model

--- models/utils.py
+++ models/utils.py
@@ -2,22 +2,23 @@ import datetime
 from typing import TypeVar, List, Generic
 
 from pydantic import BaseModel
+from pydantic.generics import GenericModel
 
 T = TypeVar('T')
 
 
-class AuditBase(BaseModel):
+class AuditBase:
     created_date: datetime
     created_by: str
     updated_date: datetime
     updated_by: str
 
 
-class IdBase(BaseModel):
+class IdBase:
     id: str
 
 
-class PaginationBase(BaseModel, Generic[T]):
+class PaginationBase(GenericModel, Generic[T]):
     total: int
     offset: int
     limit: int
